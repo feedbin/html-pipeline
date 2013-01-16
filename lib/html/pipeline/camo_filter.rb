ea9db4f0825aebecebcafa83ba29172141019dcf
Ben Ubois <ben@benubois.com>
2013-01-15 23:09:33 -0800

    Don't hexencode url before getting hash.

--- lib/html/pipeline/camo_filter.rb
+++ lib/html/pipeline/camo_filter.rb
@@ -43,8 +43,7 @@ module HTML
       # The camouflaged URL for a given image URL.
       def asset_proxy_url(url)
         url = URI::encode(URI::decode(url))
-        url = hexencode(url)
-        "#{asset_proxy_host}/#{asset_url_hash(url)}/#{url}"
+        "#{asset_proxy_host}/#{asset_url_hash(url)}/#{hexencode(url)}"
       end
 
       # Private: calculate the HMAC digest for a image source URL.
